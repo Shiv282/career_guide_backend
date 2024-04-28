1d4b70c96baab35e2bc78b921819debbea7fd1f6
Shivaraj <shivucm8055@gmail.com>
2024-04-29 02:05:57 +0530

    feat: adding flask server

---
+++ flask-backend/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+
+from app import predict,vak_predict
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    sample_input = ['2','3','6','5','0','1','1', '1', '1', '2', '0', '0','1','1','0','7','13','7','2','7','5']
+    result = predict(sample_input)
+    print("ANSWERRRRR __ ",result[0])
+    return result[0]
+
+@app.route('/careerPredict', methods=['POST'])
+def example():
+    data = request.json  # Access JSON request body
+    # Process the data...
+    result = predict(data["data"])
+    return result[0]
+
+@app.route('/vakModel', methods=['POST'])
+def vak():
+    data = request.json 
+    print(data["data"]) 
+    result = vak_predict(data["data"])
+    response = [result[0][0],np.float64(result[0][1])]
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
